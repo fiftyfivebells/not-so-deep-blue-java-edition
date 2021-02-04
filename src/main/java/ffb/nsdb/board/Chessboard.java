77134c5e46d86ee96369559eea56610cb3baca01
Stephen Bell <stephen.l.bell@gmail.com>
2021-02-04 12:42:06 -0500

    Add some private attributes

--- src/main/java/ffb/nsdb/board/Chessboard.java
+++ src/main/java/ffb/nsdb/board/Chessboard.java
@@ -2,6 +2,17 @@ package ffb.nsdb.board;
 
 public class Chessboard {
 
+  // Various bitboards for different pieces
+  private long[][] pieces;
+  private long emptySquares;
+  private long occupiedSquares;
+  private long enPassantTarget;
+
+  private byte castleAvailability;
+  private byte halfMoveClock;
+  private byte turnNumber;
+  private byte activeSide;
+
   public Chessboard() {}
 
   public Chessboard(String fen) {}
