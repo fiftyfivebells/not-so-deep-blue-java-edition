70d44112a25720082eaacf0e99ba7f28153f1b3b
Stephen Bell <stephen.l.bell@gmail.com>
2021-02-05 13:56:17 -0500

    Make some constants for convenience

--- src/main/java/ffb/nsdb/Constants.java
+++ src/main/java/ffb/nsdb/Constants.java
@@ -1,3 +1,22 @@
 package ffb.nsdb;
 
-public class Constants {}
+public class Constants {
+
+  // Important FEN strings
+  public static final String START_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
+  // Colors
+  public static final byte WHITE = 0;
+  public static final byte BLACK = 1;
+
+  // Pieces
+  public static final byte PAWN = 0;
+  public static final byte KNIGHT = 1;
+  public static final byte BISHOP = 2;
+  public static final byte ROOK = 3;
+  public static final byte QUEEN = 4;
+  public static final byte KING = 5;
+  public static final byte ALL = 6;
+
+  public static final byte NO_SQUARE = 65;
+}
