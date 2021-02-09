c5111d894a3e948d5bc1d2bb276ef171f34aaff2
Stephen <stephen.l.bell@gmail.com>
2021-02-08 22:27:21 -0500

    Create MoveConstants static class

---
+++ src/main/java/ffb/nsdb/moves/MoveConstants.java
@@ -0,0 +1,14 @@
+package ffb.nsdb.moves;
+
+public class MoveConstants {
+  public enum MOVE_FLAGS {
+    QUIET_MOVE,
+    D_PAWN_PUSH,
+    K_CASTLE,
+    Q_CASTLE,
+    CAPTURE,
+    EP_CAPTURE,
+    PROMOTION,
+    CAP_PROMOTION
+  }
+}
