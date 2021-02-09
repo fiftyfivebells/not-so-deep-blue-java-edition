91fcddff1308db9fdcae7825e541bda75e8e9c7d
Stephen <stephen.l.bell@gmail.com>
2021-02-08 20:58:38 -0500

    Create Move class and some overridden method templates

---
+++ src/main/java/ffb/nsdb/moves/Move.java
@@ -0,0 +1,18 @@
+package ffb.nsdb.moves;
+
+public class Move {
+  private static final int PIECE_BITS = 7;
+  private static final int FLAG_BITS = 7;
+
+  private int move;
+
+  @Override
+  public boolean equals(Object obj) {
+    return super.equals(obj);
+  }
+
+  @Override
+  public String toString() {
+    return super.toString();
+  }
+}
